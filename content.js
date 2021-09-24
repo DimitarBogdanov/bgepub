5ebf808682c96606370470a5634c5b60b8e74ed3
Dimitar Bogdanov <dimitar.bogdanov@yahoo.com>
2021-09-24 20:47:48 +0300

    Add files via upload

---
+++ content.js
@@ -0,0 +1,266 @@
+var content = [
+  {
+      name: 'Музика',
+      grade: 1,
+      href: 'https://www.anubis-bulvest.com/epub/html/a1muz_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Изобразително изкуство',
+      grade: 1,
+      href: 'https://www.anubis-bulvest.com/epub/html/a1izo_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Български език',
+      grade: 2,
+      href: 'https://www.anubis-bulvest.com/epub/html/a2be_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Музика',
+      grade: 2,
+      href: 'https://www.anubis-bulvest.com/epub/html/a2muz_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Изобразително изкуство',
+      grade: 2,
+      href: 'https://www.anubis-bulvest.com/epub/html/a2izo_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Български език',
+      grade: 3,
+      href: 'https://www.anubis-bulvest.com/epub/html/a3be/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Музика',
+      grade: 3,
+      href: 'https://www.anubis-bulvest.com/epub/html/a3muz/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Човекът и природата',
+      grade: 3,
+      href: 'https://www.anubis-bulvest.com/epub/html/a3chp/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Човекът и обществото',
+      grade: 3,
+      href: 'https://www.anubis-bulvest.com/epub/html/a3cho/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Изобразително изкуство',
+      grade: 3,
+      href: 'https://www.anubis-bulvest.com/epub/html/a3izo/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Български език',
+      grade: 5,
+      href: 'https://www.anubis-bulvest.com/epub/html/a5be_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Музика',
+      grade: 5,
+      href: 'https://www.anubis-bulvest.com/epub/html/a5muz_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Човекът и природата',
+      grade: 5,
+      href: 'https://www.anubis-bulvest.com/epub/html/a5chp_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'История',
+      grade: 5,
+      href: 'https://www.anubis-bulvest.com/epub/html/a5ist_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Литература',
+      grade: 5,
+      href: 'https://www.anubis-bulvest.com/epub/html/a5lit_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Изобразително изкуство',
+      grade: 5,
+      href: 'https://www.anubis-bulvest.com/epub/html/a5izo_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Технологии и предприемачество',
+      grade: 5,
+      href: 'https://www.anubis-bulvest.com/epub/html/a5tp_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Информационни технологии',
+      grade: 5,
+      href: 'https://www.anubis-bulvest.com/epub/html/a5it_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'География и икономика',
+      grade: 5,
+      href: 'https://www.anubis-bulvest.com/epub/html/a5geo_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Български език',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/a6be_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Музика',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/a6muz_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Човекът и природата',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/a6chp_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'История',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/a6ist_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Изобразително изкуство',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/a6izo_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Технологии и предприемачество',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/a6tp_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Информационни технологии',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/a6it_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'География и икономика',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/a6geo_body/',
+      publisher: 'Aнубис'
+  },
+  {
+      name: 'Музика',
+      grade: 2,
+      href: 'https://www.anubis-bulvest.com/epub/html/b2muz_body/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Изобразително изкуство',
+      grade: 2,
+      href: 'https://www.anubis-bulvest.com/epub/html/b2izo/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Технологии и предприемачество',
+      grade: 2,
+      href: 'https://www.anubis-bulvest.com/epub/html/b2tp_body/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Музика',
+      grade: 3,
+      href: 'https://www.anubis-bulvest.com/epub/html/b3muz/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Човекът и природата',
+      grade: 3,
+      href: 'https://www.anubis-bulvest.com/epub/html/b3chp/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Човекът и обществото',
+      grade: 3,
+      href: 'https://www.anubis-bulvest.com/epub/html/b3cho/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Изобразително изкуство',
+      grade: 3,
+      href: 'https://www.anubis-bulvest.com/epub/html/b3izo/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Технологии и предприемачество',
+      grade: 3,
+      href: 'https://www.anubis-bulvest.com/epub/html/b3tp/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Човекът и обществото',
+      grade: 4,
+      href: 'https://www.anubis-bulvest.com/epub/html/b4cho_body/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Български език',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/b6be_body/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Музика',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/b6muz_body/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Човекът и природата',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/b6chp_body/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'История',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/b6ist_body/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Литература',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/b6lit_body/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Изобразително изкуство',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/b6izo_body/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'Информационни технологии',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/b6it_body/',
+      publisher: 'Булвест 2000'
+  },
+  {
+      name: 'География и икономика',
+      grade: 6,
+      href: 'https://www.anubis-bulvest.com/epub/html/b6geo_body/',
+      publisher: 'Булвест 2000'
+  }
+]
